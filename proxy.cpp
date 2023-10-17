23517533da8219d8f0398365e31a1bf6ff7908a8
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2023-10-16 22:26:53 -0600

    Update proxy.cpp

--- proxy.cpp
+++ proxy.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <regex>
 
 int initServerSock(struct sockaddr_in address){
     int serv;
@@ -23,23 +24,26 @@ int initServerSock(struct sockaddr_in address){
         exit(EXIT_FAILURE);
     }
 		std::cout << "Server is listening on port 8080..." << std::endl;
-
     return serv;
 }
 
 int initClientSock(struct sockaddr_in serv_addr){
     int sock;
     // Creating socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "Socket creation error" << std::endl;
+    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        std::cout << "Scoket creation error" << std::endl;
         return -1;
     }
 
-    // Connecting to the server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(80);
+    inet_pton(AF_INET, "136.159.2.17", &serv_addr.sin_addr);
+
+    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         std::cout << "Connection Failed" << std::endl;
         return -1;
     }
+    std::cout << "Client Connected" << std::endl;
     return sock;
 }
 
@@ -49,6 +53,7 @@ int acceptSocket(struct sockaddr_in address, int server_fd, int addrlen){
         perror("accept");
         exit(EXIT_FAILURE);
     }
+    std::cout << "Socket Accepted!" << std::endl;
     return new_socket;
 }
 
@@ -62,33 +67,54 @@ int main() {
     address.sin_family = AF_INET; // Address family (IPv4)
     address.sin_addr.s_addr = INADDR_ANY; // Listen on all available interfaces
     address.sin_port = htons(8080); // Port number, converted to network byte order
-
+    //inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
     server_fd = initServerSock(address);
 
     return_sock = acceptSocket(address, server_fd, addrlen);
 
-    serv_addr.sin_family = AF_INET; // Address family (IPv4)
-    serv_addr.sin_port = htons(80); // Port number to connect to, converted to network byte order
-	inet_pton(AF_INET, "136.159.5.25", &serv_addr.sin_addr); // Converting IPv4 and IPv6 addresses from text to binary form
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(80);
+    inet_pton(AF_INET, "136.159.2.17", &serv_addr.sin_addr);
 
     client_sock = initClientSock(serv_addr);
 
     while(1){
-
     // Reading the message from the client and printing it to the console
     ssize_t bytesRead = read(return_sock, buffer, 4096);
 		if (bytesRead < 0) {
-        perror("read");
-        exit(EXIT_FAILURE);
+        //perror("read");
+        //exit(EXIT_FAILURE);
     }
+
+
+    // std::string s = buffer;
+
+    // std::regex hostReg("Host: ");
+    // auto words_begin = std::sregex_iterator(s.begin(), s.end(), hostReg);
+    // std::regex endHostReg(".ca\r\n");
+    // auto end = std::sregex_iterator(s.begin(), s.end(), endHostReg);
+    // std::cout << std::distance(words_begin, end) << std::endl;
+    // for(std::sregex_iterator i = words_begin; i != end; ++i){
+    //     std::smatch match = *i;
+    //     std::string match_str = match.str();
+    //     std::cout << match_str << std::endl;
+    // }
+    
+
+
+
     std::cout << "Message received: " << buffer << std::endl;
 
-    send(client_sock, buffer, 4096,0);
+    if(buffer[0] == 'P'){continue;}
 
+    send(client_sock, buffer, 4096,0);
+    std::cout << "Request to website sent!" << std::endl;
     read(client_sock, buffer1, 4096);
+    std::cout << "Message Recieved from website!" << std::endl;
 
     // Sending a response to the client
     send(return_sock, buffer1, 4096, 0);
+    //send(return_sock, "\r\n\r\n", strlen("\r\n\r\n"),0);
     std::cout << "Hello message sent" << std::endl;
     }
     
